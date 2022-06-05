b6c45b9898753d1207e253b1ea76ee3b9e356a78
peiyu <peiyu0626.com>
2022-06-05 09:52:38 +0800

    first commit

--- src/main/java/com/tit.java
+++ src/main/java/com/tit.java
@@ -7,5 +7,7 @@ package com;
 public class tit {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello new world!");
+        System.out.println("hello new world2!");
     }
 }
